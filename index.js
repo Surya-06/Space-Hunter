125b2456e2372b2769226b30811941c837540f5a
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2018-12-14 20:07:53 +0530

    battle server init

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('view engine' , 'ejs' );
+
+PORT = process.env.PORT || process.argv[2] || 8080 ;
+
+app.get ( '/' , (req,res)=>{
+    res.render ( 'index.ejs' , {PORT : PORT} );
+});
+
+io.on('connection' , (socket)=>{
+    console.log ( "Client connected " , socket.id );
+    socket.on ( 'message' , (msg)=>{
+        console.log ( "CLIENT : " , msg );
+        
+    });
+})
+
+http.listen( PORT, ()=> {console.log("Server up at : " , PORT );});
