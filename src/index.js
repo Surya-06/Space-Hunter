3285cd95bda4dbc5f4942dc4cea396ceb0db6141
Surya-06 <susarla.surya.prakash.1998@gmail.com>
2018-12-29 17:06:42 +0530

    Working model with bullets and input !

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import player_img from './assets/player_ship.png';
 import opp_img from './assets/opponent_ship.png';
 import bullet_img from './assets/bullet.png';
 
-const WIDTH = 800
-const HEIGHT = 600 
-const MOVEMENT_PIX = 30 ;
+const WIDTH = 800;
+const HEIGHT = 600; 
+const MOVEMENT_PIX = 30;
 const RIGHT_ARROW = 39;
 const LEFT_ARROW = 37;
-const SPACE = 32 ;
+const SPACE = 32;
+const BULLET_SPEED = 5;
+const BULLET_WIDTH = 10;
+const BULLET_HEIGHT = 30;
 
-var canvas , context , player , opp ;
+
+var canvas , context , player , opp , bulletImage ;
+var bulletList = [] ;
+
+class Bullet {
+    constructor ( x , y ){
+        this.x = x ;
+        this.y = y ;
+    }
+};
 
 function ActivateInputListeners () {    
     document.addEventListener ( 'keydown' , (event) => {
@@ -30,31 +42,32 @@ function ActivateInputListeners () {
         }
         if ( event.keyCode == SPACE ){
             // space key
-            console.log ( "Adding bullet to list" );
+            console.log ( "Adding bullet to list at position : " , player.x , " " , player.y-BULLET_SPEED );
+            bulletList.push ( new Bullet ( player.x + (player.width / 2) - 5  , player.y-BULLET_SPEED ) );
         }
     });
 }
 
-var init_y = 450 ;
-
 function main () {
+
     // Main loop for printing all the game components
-    console.log ( "Drawing the models" ); 
     context.clearRect ( 0,0, WIDTH , HEIGHT );
+
     // Drawing the player's image 
     context.drawImage ( player.model , player.x , player.y, player.width , player.height );
+
     // Drawing the opponent's image 
     context.drawImage ( opp.model , opp.x , opp.y , opp.width , opp.height );
 
-    // testing bullet 
-    var temp = new Image();
-    temp.src = bullet_img ;
-    if ( init_y > 60 ){
-        init_y-=1;
+    // Update bullet values 
+    for ( var i=0 ; i<bulletList.length ; i++ )
+        if ( bulletList[i].y > 0 )  bulletList[i].y -= BULLET_SPEED;
+        else   bulletList.splice ( i , 1 );
+
+    // Redraw the bullets 
+    for ( var i=0 ; i<bulletList.length ; i++ ){
+        context.drawImage ( bulletImage , bulletList[i].x , bulletList[i].y , BULLET_WIDTH , BULLET_HEIGHT );
     }
-    else 
-        init_y = 450 ;
-    context.drawImage ( temp , 150 , init_y , 10,30 );
 
     requestAnimationFrame ( main );
 }
@@ -65,7 +78,7 @@ function init() {
     context = canvas.getContext('2d');
     var playerImage = new Image();
     var oppImage = new Image();
-    var bulletImage = new Image();
+    bulletImage = new Image();
     playerImage.src = player_img;
     oppImage.src = opp_img;
     bulletImage.src = bullet_img;
@@ -83,8 +96,7 @@ function init() {
         height : 100 ,
         model : oppImage
     };
-
-    // testing 
+    // Activate input listeners 
     ActivateInputListeners();
 
     requestAnimationFrame ( main );
